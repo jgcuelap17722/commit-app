323e4c6e50d4f60beb7af24cc1473faf5d2c2638
jose_cuela <jose.cuela@mediabyte.com.pe>
2023-10-08 18:47:27 -0500

    feature: add globals environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommitModule } from './commit/commit.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommitModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    CommitModule,
+  ],
   controllers: [],
   providers: [],
 })
