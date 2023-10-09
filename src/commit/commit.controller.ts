baefad92698613b274f9e77a0eacae3c9c1b21cf
jose_cuela <jose.cuela@mediabyte.com.pe>
2023-10-08 19:10:05 -0500

    feature: add swagger for project

--- src/commit/commit.controller.ts
+++ src/commit/commit.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { CommitService } from './commit.service';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('Commit')
 @Controller('commit')
 export class CommitController {
   constructor(private readonly commitService: CommitService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { setupSwagger } from './utils/setup-swagger';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  setupSwagger(app);
 
   await app.listen(3000, () => {
     logger.log('Api Server is listening on 3000');
