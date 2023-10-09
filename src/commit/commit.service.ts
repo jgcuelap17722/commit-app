10a3db0b274ceb697d4ff6ccd6499946d0076897
jose_cuela <jose.cuela@mediabyte.com.pe>
2023-10-08 19:06:12 -0500

    feature: add getListCommit for service

--- src/commit/commit.service.ts
+++ src/commit/commit.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { GetListCommitUsecase } from './application/usecases/get-list-commit.usecase';
 
 @Injectable()
-export class CommitService {}
+export class CommitService {
+  constructor(private readonly _getListCommitUsecase: GetListCommitUsecase) {}
+  async getListCommit() {
+    return await this._getListCommitUsecase.handle();
+  }
+}
