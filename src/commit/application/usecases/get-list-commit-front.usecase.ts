e2db4470719b797734a1892e6d883d8c059362fc
jose cuela <jgcuelap@gmail.com>
2023-10-13 12:26:03 -0500

    feature: add queries in usecase get list front

--- src/commit/application/usecases/get-list-commit-front.usecase.ts
+++ src/commit/application/usecases/get-list-commit-front.usecase.ts
@@ -1,15 +1,34 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { QueriesListCommitDto } from 'src/commit/dto/query-get-list-commit-back.dto';
 import { GithubApiService } from 'src/shared/services/github-api.service';
 
 @Injectable()
 export class GetListCommitFrontUsecase {
   constructor(private readonly _githubApiService: GithubApiService) {}
-  async handle() {
+  async handle(queries: QueriesListCommitDto) {
     try {
+      const { page, limit } = queries;
+
       const { GITHUB_API_URL_COMMITS_FRONT } = process.env;
-      return await this._githubApiService.getCommits(
+
+      const listCommitsBack = await this._githubApiService.getCommits(
         GITHUB_API_URL_COMMITS_FRONT,
       );
+
+      const skip = (page - 1) * limit;
+      const totalItems = listCommitsBack.length;
+      const totalPages = Math.ceil(totalItems / limit);
+      const listCommitFiltered = listCommitsBack.slice(skip, skip + limit);
+
+      return {
+        success: true,
+        data: listCommitFiltered,
+        pagination: {
+          currentPage: Number(page),
+          totalPages,
+          totalItems,
+        },
+      };
     } catch (e) {
       throw new InternalServerErrorException({
         error: {
