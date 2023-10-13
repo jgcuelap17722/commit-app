7393f7e8f8e33fc484691fbfd881c3930e90cb9f
jose cuela <jgcuelap@gmail.com>
2023-10-13 12:26:40 -0500

    hotfix: update controller with news queries

--- src/commit/commit.controller.ts
+++ src/commit/commit.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { CommitService } from './commit.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { QueriesListCommitDto } from './dto/query-get-list-commit-back.dto';
+import { ListCommitResponse } from './dto/list-commit-response.dto';
 @ApiTags('Commit')
 @Controller('commit')
 export class CommitController {
   constructor(private readonly commitService: CommitService) {}
 
+  @ApiOperation({
+    description: 'Lista de commits confirmados del backend.',
+  })
+  @ApiOkResponse({
+    type: ListCommitResponse,
+  })
   @Get('list-commit-back')
-  async getListCommitBack() {
-    return await this.commitService.getListCommitBack();
+  @HttpCode(HttpStatus.OK)
+  async getListCommitBack(@Query() queries: QueriesListCommitDto) {
+    return await this.commitService.getListCommitBack(queries);
   }
 
+  @ApiOperation({
+    description: 'Lista de commits confirmados del frontend.',
+  })
+  @ApiOkResponse({
+    type: ListCommitResponse,
+  })
   @Get('list-commit-front')
-  async getListCommitFront() {
-    return await this.commitService.getListCommitFront();
+  @HttpCode(HttpStatus.OK)
+  async getListCommitFront(@Query() queries: QueriesListCommitDto) {
+    return await this.commitService.getListCommitFront(queries);
   }
 }
